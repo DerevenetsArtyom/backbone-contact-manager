f7db46f3ecea0f0cedfbda40ffeca3b8389fcd06
DerevenetsArtyom <derevenetsartyom@gmail.com>
2018-05-11 21:46:18 +0300

    Added a couple of callback for events

--- todos.js
+++ todos.js
@@ -68,6 +68,31 @@ $(function(){
           this.model.on('destroy', this.remomve, this);
       },
 
+      // Re-render the titles of the to-do item.
+      render: function() {
+          this.$el.html(this.template(this.model.toJSON()));
+          this.$el.toggleClass('done', this.model.get('done'));
+          this.input = this.$('.edit');
+          return this;
+      },
+
+      // Toggle the `"done"` state of the model.  --> "click .toggle" event
+      toggleDone: function() {
+          this.model.toggle();
+      },
+
+      // Switch view into `"editing"` mode, displaying the input field.
+      // "dblclick .view" event
+      edit: function() {
+          this.$el.addClass("editing");
+          this.input.focus();
+      },
+
+
+
+
+
+
 
 
 
