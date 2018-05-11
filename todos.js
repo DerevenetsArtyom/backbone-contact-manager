039c420123790982abc4fc6c082d1b535cac55ab
DerevenetsArtyom <derevenetsartyom@gmail.com>
2018-05-11 22:26:28 +0300

    Created another event handlers for AppView

--- todos.js
+++ todos.js
@@ -116,70 +116,89 @@ $(function(){
     // Overall **AppView** is the top-level piece of UI.
     const AppView = Backbone.View.extend({
 
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
-    el: $("#todoapp"),
+        // Instead of generating a new element, bind to the existing skeleton of
+        // the App already present in the HTML.
+        el: $("#todoapp"),
 
-    // Template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
+        // Template for the line of statistics at the bottom of the app.
+        statsTemplate: _.template($('#stats-template').html()),
 
-    // Delegated events for creating new items, and clearing completed ones.
-    events: {
-      "keypress #new-todo":  "createOnEnter",
-      "click #clear-completed": "clearCompleted",
-      "click #toggle-all": "toggleAllComplete"
-    },
+        // Delegated events for creating new items, and clearing completed ones.
+        events: {
+          "keypress #new-todo":  "createOnEnter",
+          "click #clear-completed": "clearCompleted",
+          "click #toggle-all": "toggleAllComplete"
+        },
 
-    // At initialization we bind to the relevant events on the `Todos`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting todos that might be saved in *localStorage*.
-    initialize: function() {
-        this.input = this.$("#new-todo");
-        this.allCheckbox = this.$("#toggle-all")[0];
+        // At initialization we bind to the relevant events on the `Todos`
+        // collection, when items are added or changed. Kick things off by
+        // loading any preexisting todos that might be saved in *localStorage*.
+        initialize: function() {
+            this.input = this.$("#new-todo");
+            this.allCheckbox = this.$("#toggle-all")[0];
 
-        this.listenTo(Todos, 'add', this.addOne);
-        this.listenTo(Todos, 'reset', this.addAll);
-        this.listenTo(Todos, 'all', this.render);
+            this.listenTo(Todos, 'add', this.addOne);
+            this.listenTo(Todos, 'reset', this.addAll);
+            this.listenTo(Todos, 'all', this.render);
 
-        this.footer = this.$('footer');
-        this.main = $('#main');
+            this.footer = this.$('footer');
+            this.main = $('#main');
 
-        Todos.fetch();
-    },
+            Todos.fetch();
+        },
 
-    // Re-rendering the App just means refreshing the statistics --
-    // the rest of the app doesn't change.
-    render: function() {
-        const done = Todos.done().length;
-        const remaining = Todos.remaining().length;
+        // Re-rendering the App just means refreshing the statistics --
+        // the rest of the app doesn't change.
+        render: function() {
+            const done = Todos.done().length;
+            const remaining = Todos.remaining().length;
 
-        if (Todos.length) {
-            this.main.show();
-            this.footer.show();
-            this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
-        } else {
-            this.main.hide();
-            this.footer.hide();
-        }
+            if (Todos.length) {
+                this.main.show();
+                this.footer.show();
+                this.footer.html(this.statsTemplate({done: done, remaining: remaining}));
+            } else {
+                this.main.hide();
+                this.footer.hide();
+            }
 
-        this.allCheckbox.checked = !remaining;
-    },
+            this.allCheckbox.checked = !remaining;
+        },
 
+        // Add a single to-do item to the list by creating a view for it, and
+        // appending its element to the `<ul>`.
+        addOne: function(todo) {
+            const view = new TodoView({model: todo});
+            this.$("#todo-list").append(view.render().el);
+        },
 
+        // Add all items in the **Todos** collection at once.
+        addAll: function() {
+            Todos.each(this.addOne, this);
+        },
 
+        // If you hit return in the main input field,
+        // create new To-do model, persisting it to *localStorage*.
+        createOnEnter: function(e) {
+            if (e.keyCode !== 13) return;
+            if (!this.input.val()) return;
 
+            Todos.create({title: this.input.val()});
+            this.input.val('');
+        },
 
+        // Clear all done to-do items, destroying their models.
+        clearCompleted: function() {
+            _.invoke(Todos.done(), 'destroy');
+            return false;
+        },
 
+        toggleAllComplete: function () {
+        const done = this.allCheckbox.checked;
+        Todos.each(function (todo) { todo.save({'done': done}); });
+    }
+  });
 
-
-
-
-
-
-
-
-
-
-
-
+    // Finally, we kick things off by creating the **App**.
+    const App = new AppView;
 });
